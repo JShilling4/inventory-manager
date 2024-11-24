c3891ecd6e73176918cd0535b61a6b8e69d56d8e
Justin Shilling <loveofmusic25@hotmail.com>
2024-11-24 16:20:25 -0500

    add quasar

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { createApp } from "vue";
+import { Notify, Quasar } from "quasar";
 import App from "./App.vue";
 import router from "@/router";
+import iconSet from "quasar/icon-set/fontawesome-v6";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
+import "quasar/dist/quasar.css";
 import { initFaLibrary, FontAwesomeIcon } from "@/plugins/fontAwesome";
-const app = createApp(App);
 
 initFaLibrary();
 
+const app = createApp(App);
+
 app.use(router);
 app.component("FontAwesomeIcon", FontAwesomeIcon);
+app.use(Quasar, {
+  plugins: { Notify },
+  iconSet: iconSet,
+  config: {
+    notify: {
+      position: "top",
+    },
+  },
+});
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/tests/vitest-setup.ts"],
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+    },
+  },
+  plugins: [
+    vue({ template: { transformAssetUrls } }),
+    quasar({ autoImportComponentCase: "pascal" }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+    extensions: [".js", ".json", ".ts", ".vue"],
   },
   css: {
     devSourcemap: true,
