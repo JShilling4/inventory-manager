59a4d29a8e823ad1038e9d74ff60b4ccb58ca44e
Justin Shilling <loveofmusic25@hotmail.com>
2024-11-26 14:16:04 -0500

    add FormSelect

--- src/components/ui/FormSelect/FormSelect.vue
+++ src/components/ui/FormSelect/FormSelect.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
-
 <template>
-  <select id="" name=""></select>
+  <QSelect
+    v-model="model"
+    v-bind="$attrs"
+    behavior="menu"
+    clearable
+    class="app-select-filter col"
+  >
+    <template #selected-item="scope">
+      <slot name="selected-item" v-bind="scope">
+        <span>{{ model }}</span>
+      </slot>
+    </template>
+  </QSelect>
 </template>
 
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+const model = defineModel<unknown>({ required: true });
+</script>
+
+<style lang="scss" scoped>
+#app .app-select-filter {
+  max-width: 315px;
+}
+</style>
