708b6e2c55e536112f8b603c94f5a8ca8b505d63
Justin Shilling <loveofmusic25@hotmail.com>
2024-11-28 12:28:08 -0500

    add category table columns and delete

--- src/views/Categories/Categories.vue
+++ src/views/Categories/Categories.vue
@@ -7,12 +7,39 @@ import { useCategoryStore } from "@/stores";
 import clone from "lodash/clone";
 import { ICON_NAMES } from "@/constants";
 import { type Category, NewCategory } from "@/types";
+import { QTableProps } from "quasar";
 
 const categoryStore = useCategoryStore();
 
 const showModal = ref(false);
 const modalAction = ref<"Add" | "Edit">("Add");
 const localCategory = ref<Category>(NewCategory());
+
+const categoryTableColumns: QTableProps["columns"] = [
+  {
+    name: "name",
+    required: true,
+    label: "Name",
+    align: "left",
+    field: (row: Category) => row.name,
+    sortable: true,
+  },
+  {
+    name: "description",
+    required: true,
+    label: "Description",
+    align: "left",
+    field: (row: Category) => row.description,
+    sortable: false,
+  },
+  {
+    name: "delete",
+    label: "",
+    align: "center",
+    field: "",
+    sortable: false,
+  },
+];
 
 function onAddCategoryClick() {
   modalAction.value = "Add";
@@ -27,9 +54,10 @@ function onEditCategoryClick(_event: Event, row: Category) {
   showModal.value = true;
 }
 
-// function onDeleteCategoryClick(id: number) {
-//   customerStore.deleteSong(id);
-// }
+function onDeleteCategoryClick(row: Category) {
+  if (!row.id) return;
+  categoryStore.deleteCategory(row.id);
+}
 
 function onHideModal() {
   localCategory.value = NewCategory();
@@ -48,12 +76,25 @@ onMounted(async () => {
     </AppButton>
     <QTable
       :rows="categoryStore.categories"
+      :columns="categoryTableColumns"
       class="customer-table mt-sm"
       :loading="categoryStore.loading"
       @row-click="onEditCategoryClick"
     >
       <template #loading>
         <QInnerLoading showing color="primary" />
+      </template>
+
+      <template #body-cell-delete="props">
+        <QTd :props="props" auto-width>
+          <QBtn
+            :icon="ICON_NAMES.Delete"
+            color="red-7"
+            size="sm"
+            rounded
+            @click.stop="onDeleteCategoryClick(props.row)"
+          />
+        </QTd>
       </template>
     </QTable>
   </PageContentContainer>
